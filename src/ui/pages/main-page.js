f5b9350a74f5e85ea8223a29f959e208c374d307
Clapa Lucian <lucian@dealdash.com>
2018-03-14 23:51:45 +0200

    add tabs

--- src/ui/pages/main-page.js
+++ src/ui/pages/main-page.js
@@ -1,20 +1,47 @@
 import React from 'react';
-import {FlatList, ImageBackground, Text, View} from "react-native";
+import {FlatList, ImageBackground, Text, View, StyleSheet, Dimensions} from "react-native";
 import {connect} from "react-redux";
 import * as uiActions from "../../redux/actions/ui";
 import SearchBar from 'react-native-searchbar';
 import * as Constants from "../../utils/constants";
+import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
+
+const initialLayout = {
+    height: 0,
+    width: Dimensions.get('window').width,
+};
+const PhotosRoute = () => <View style={[ styles.container, { backgroundColor: '#ff4081' } ]} />;
+const PeopleRoute = () => <View style={[ styles.container, { backgroundColor: '#673ab7' } ]} />;
+const GroupsRoute = () => <View style={[ styles.container, { backgroundColor: '#FFFFB0' } ]} />;
 
 class MainPage extends React.Component {
+
+    state = {
+        index: 0,
+        routes: [
+            { key: 'first', title: 'First' },
+            { key: 'second', title: 'Second' },
+            { key: 'third', title: 'third' },
+        ],
+    };
 
     componentDidMount() {
         this.props.onMount();
     }
 
     handleChangeText = (input) => {
-        console.log("luci", input);
         this.props.onSearch(input);
     };
+
+    _handleIndexChange = index => this.setState({ index });
+
+    _renderHeader = props => <TabBar {...props} />;
+
+    _renderScene = SceneMap({
+        first: PhotosRoute,
+        second: PeopleRoute,
+        third: GroupsRoute
+    });
 
     render() {
         return (
@@ -25,27 +52,43 @@ class MainPage extends React.Component {
                     hideBack
                 />
 
-                <FlatList
-                    style={{flex: 1, marginTop: 100}}
-                    data={this.props.photos}
-                    renderItem={(item) => {
-                        return <View style={{borderBottomColor: 'black', borderBottomWidth: 1, height: 100}}>
-                            <ImageBackground
-                                style={{width: '100%', height: 300}}
-                                source={{uri: Constants.getPhotoUrl(item.item)}}
-                            />
-                        </View>
-                    }}
-                    keyExtractor={(item, index) => {
-                        return index;
-                    }}
-
+                <TabViewAnimated
+                    style={styles.container}
+                    navigationState={this.state}
+                    renderScene={this._renderScene}
+                    renderHeader={this._renderHeader}
+                    onIndexChange={this._handleIndexChange}
+                    initialLayout={initialLayout}
                 />
+
+                {/*<FlatList*/}
+                    {/*style={{flex: 1, marginTop: 100}}*/}
+                    {/*data={this.props.photos}*/}
+                    {/*renderItem={(item) => {*/}
+                        {/*return <View style={{borderBottomColor: 'black', borderBottomWidth: 1, height: 250, paddingLeft: 4, paddingBottom: 4, paddingRight: 4}}>*/}
+                            {/*<ImageBackground*/}
+                                {/*style={{flex: 1, flexDirection: 'row'}}*/}
+                                {/*source={{uri: Constants.getPhotoUrl(item.item)}}*/}
+                            {/*/>*/}
+                        {/*</View>*/}
+                    {/*}}*/}
+                    {/*keyExtractor={(item, index) => {*/}
+                        {/*return index;*/}
+                    {/*}}*/}
+
+                {/*/>*/}
             </View>
         )
     }
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 100,
+        flex: 1,
+    },
+});
 
 const mapStateToProps = state => {
     return {
