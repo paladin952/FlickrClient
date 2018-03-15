93e97b8d6bd6463247e96c2a65bc0b4c8e29099e
Clapa Lucian <lucian@dealdash.com>
2018-03-15 22:36:44 +0200

    remove switching tabs code

--- src/redux/middleswares/ui.js
+++ src/redux/middleswares/ui.js
@@ -2,8 +2,6 @@ import * as actions from "../consts/action-types";
 import * as uiActions from "../actions/ui";
 import * as photosActions from "../actions/photo";
 import * as groupsActions from "../actions/group";
-import * as dataActions from "../actions/data";
-import {TAB_INDEX_GROUPS, TAB_INDEX_PHOTOS} from "../../utils/constants";
 
 const uiMiddleware = ({dispatch, getState}) => next => action => {
     if (action.type !== actions.ON_SEARCH && action.type !== actions.LOAD_MORE_PHOTOS && action.type !== actions.LOAD_MORE_GROUPS
@@ -14,7 +12,8 @@ const uiMiddleware = ({dispatch, getState}) => next => action => {
     let {type, payload} = action;
     if (type === actions.ON_SEARCH) {
         if (payload && payload.length > 0) {
-            dispatch(dataActions.fetchData(getState().ui.currentTabIndex || 0, payload));
+            dispatch(photosActions.fetchPhotos(payload));
+            dispatch(groupsActions.fetchGroups(payload));
         } else {
             dispatch(uiActions.onClearSearch())
         }
@@ -35,15 +34,6 @@ const uiMiddleware = ({dispatch, getState}) => next => action => {
             dispatch(groupsActions.fetchGroups(getState().ui.searchText, getState().group.groupsCurrentPage + 1));
             dispatch(groupsActions.incrementGroupsPage())
         }
-    }
-
-    if (type === actions.TAB_NEW_INDEX && getState().ui.searchText) {
-        if (action.payload === TAB_INDEX_PHOTOS && getState().photo.photos.length === 0) {
-            dispatch(dataActions.fetchData(TAB_INDEX_PHOTOS, payload));
-        } else if (action.payload === TAB_INDEX_GROUPS && getState().group.groups.length === 0) {
-            dispatch(dataActions.fetchData(TAB_INDEX_GROUPS, payload));
-        }
-        next(action);
     }
 };
 
