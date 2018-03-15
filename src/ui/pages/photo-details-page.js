547f507b898e99f2240284f135dd22dc25f124d4
Clapa Lucian <lucian@dealdash.com>
2018-03-15 15:06:24 +0200

    allow only portrait

--- src/ui/pages/photo-details-page.js
+++ src/ui/pages/photo-details-page.js
@@ -4,9 +4,7 @@ import {Button, Text} from "native-base";
 
 
 const PhotoDetailsPage = (props) => {
-    console.log('dadadada', props);
     let uri = props.navigation.state.params.uri;
-    console.log('dadadada uri', uri);
     return <View style={{flex: 1}}>
         <Image style={{flex: 1}} source={{uri: uri}} resizeMode={'contain'}/>
     </View>
